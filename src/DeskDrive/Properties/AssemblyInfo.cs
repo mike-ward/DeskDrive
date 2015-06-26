d76984347f92a5a104ad324f14568216f78a12de
mike-ward <mike@mike-ward.net>
2015-06-25 20:30:48 -0400

    2.1.2

--- src/DeskDrive/Properties/AssemblyInfo.cs
+++ src/DeskDrive/Properties/AssemblyInfo.cs
@@ -1,27 +1,19 @@
-﻿// Copyright (c) 2009 Blue Onion Software
-// All rights reserved
-
-using System;
+﻿using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
 
 [assembly: AssemblyTitle("Desk Drive")]
 [assembly: AssemblyDescription("Automatic drive/media shortcuts on your desktop")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mike Ward")]
 [assembly: AssemblyProduct("DeskDrive")]
-[assembly: AssemblyCopyright("Copyright © Mike Ward 2013")]
+[assembly: AssemblyCopyright("Copyright © Mike Ward 2015")]
 [assembly: AssemblyTrademark("Desk Drive")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-
 [assembly: Guid("855c8a64-bc33-46ba-9dfe-345eabdc9725")]
-
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
-
-[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: NeutralResourcesLanguage("en")]
--- src/deskdrive-package/tools/ChocolateyInstall.ps1
+++ src/deskdrive-package/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'deskdrive' 'exe' '/SILENT' 'https://github.com/mike-ward/DeskDrive/releases/download/v2.1.1/DeskDriveSetup.exe'
+Install-ChocolateyPackage 'deskdrive' 'exe' '/SILENT' 'https://github.com/mike-ward/DeskDrive/releases/download/v2.1.2/DeskDriveSetup.msi'
